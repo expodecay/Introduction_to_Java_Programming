f3afb5b022f3f6d1ea4ece9e6744f1247a234005
Rick <rick.ramirez6174@gmail.com>
2015-09-13 18:32:55 -0700

    delete commit

---
+++ src/chapter_9/DELETE.java
@@ -0,0 +1,10 @@
+package chapter_9;
+
+/**
+ * Created by Rick on 9/13/2015.
+ */
+public class DELETE {
+    public static void main(String[] args) {
+        System.out.println("What the hell");
+    }
+}
